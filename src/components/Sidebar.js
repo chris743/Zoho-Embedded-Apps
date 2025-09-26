2c4ec54bcdac81ff1c6ee1f4976937d90fe27caa
Chris Marshall <chris@cobblestonefruit.com>
2025-09-26 10:24:08 -0700

    added user settings to sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -65,7 +65,6 @@ const menuItems = [
                 id: 'users',
                 label: 'User Management',
                 path: '/users',
-                requiresRole: ['admin']
             },
             {
                 id: 'placeholder-growers',
